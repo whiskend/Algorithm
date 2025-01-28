f585fd70a7c2a8e0b4b4e97ade1b47ed605310f4
이경근 <113527535+whiskend@users.noreply.github.com>
2025-01-28 17:16:32 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,26 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int sco = 0;
-    
+int main() {
+    int sco;
     cin >> sco;
-    
-    if(90 <= sco && sco <= 100){
-        cout<< "A" <<endl;
-    }
-    else if(80 <= sco && sco <= 89){
-        cout<< "B" <<endl;
-    }
-    else if(70 <= sco && sco <= 79){
-        cout<< "C" <<endl;
-    }
-    else if(60 <= sco && sco <= 69){
-        cout<< "D" <<endl;
-    }
-    else{
-        cout<< "F" << endl;
-    }
-    
+
+    if (sco >= 90) cout << "A";
+    else if (sco >= 80) cout << "B";
+    else if (sco >= 70) cout << "C";
+    else if (sco >= 60) cout << "D";
+    else cout << "F";
+
     return 0;
 }
