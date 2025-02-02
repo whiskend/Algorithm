320c9cc4f3fe0fb2f64311d9dd6b3013fad1de97
이경근 <113527535+whiskend@users.noreply.github.com>
2025-02-03 01:26:39 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int T = 0;
+    cin >> T;
+
+    for (int i = 0; i < T; i++){
+        int A,B = 0;
+        cin >> A >> B;
+        cout << "Case #" << i+1 << ": " << A+B <<endl;
+    }
+}
