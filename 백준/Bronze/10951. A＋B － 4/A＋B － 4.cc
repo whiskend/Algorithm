bf2fc75839c69f57f07c30db3a7c0a407c26dd91
이경근 <113527535+whiskend@users.noreply.github.com>
2025-02-06 15:43:22 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int A, B = 0;
+    while(cin >> A >> B){
+        cout << A + B << endl;
+    };
+
+    return 0;
+}
