27ac011b4dceeae2e1ed90eea00f2a5f88946661
이경근 <113527535+whiskend@users.noreply.github.com>
2025-02-21 17:54:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char in = '\0';
+    cin >> in;
+    cout << (int)in;
+    
+    return 0;
+}
