a350a696f0870beca384fec5452ff1eb8d8fe8f2
이경근 <113527535+whiskend@users.noreply.github.com>
2025-04-07 15:04:33 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A*B;
+    
+    return 0;
+}
